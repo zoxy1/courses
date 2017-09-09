83c44d9a817dfcbc3f7ee969ef10740a90b15f6f
zoxy1 <zoxy1@yandex.ru>
2017-09-09 15:17:28 +0700

    Added Hello World

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+/**
+ * Calculate.
+ *
+ * @author Andrey Kudryavtsev (zoxy1@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+	 /**
+     * Main.
+     * @param args - args.
+     */
+	public static void main(String[] arg) {
+		System.out.println("Hello world");
+	}
+}
