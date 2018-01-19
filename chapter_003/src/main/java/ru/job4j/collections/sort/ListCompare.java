bc170a8473857aeacdcdf828d53fa7290d4260bf
zoxy1 <zoxy1@yandex.ru>
2018-01-19 18:46:20 +0700

    completed ListCompare class

--- chapter_003/src/main/java/ru/job4j/collections/sort/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/collections/sort/ListCompare.java
@@ -9,16 +9,23 @@ public class ListCompare implements Comparator<List<Integer>> {
         int result;
         if (left.size() < right.size()) {
             result = -1;
+        } else {
+            result = 1;
         }
-
         if (left.size() == right.size()) {
+            result = 0;
             for (int i = 0; i < left.size(); i++) {
-                if (left.get(i) < right.get(i)) {
-                    result = -1;
+                if (left.get(i) != right.get(i)) {
+                    if (left.get(i) < right.get(i)) {
+                        result = -1;
+                    } else {
+                        result = 1;
+                    }
+                    break;
                 }
             }
         }
-        return left;
+        return result;
     }
 }
 
